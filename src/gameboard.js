f4c1f9a2968169fb67da7fc14571ea7afd6e7d79
Romain Gremaud <rgremau@gmail.com>
2026-02-25 17:55:49 -0500

    Add player files

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,24 +1,3 @@
-/*
-Create a Gameboard class/factory.
-
-Note that we have not yet created any User Interface. 
-We should know our code is coming together by running the tests. 
-Done:
-You shouldn’t be relying on console.log or DOM methods to make sure your code is doing what you expect it to.
-Gameboards should be able to place ships at specific coordinates by calling the ship factory or class.
-
-To do:
-Gameboards should have a receiveAttack function that takes a pair of coordinates, 
-determines whether or not the attack hit a ship and then sends the ‘hit’ function to the correct ship, 
-or records the coordinates of the missed shot.
-Gameboards should keep track of missed attacks so they can display them properly.
-Gameboards should be able to report whether or not all of their ships have been sunk.
-
-10x10 board
-ships: two sets of 5, 4, 3, 3, 2 - tie to players
-
-*/
-
 export class Gameboard {
   constructor() {
     // build an array of 10x10
---
+++ src/player.js
@@ -0,0 +1,9 @@
+/*
+Create a Player class/factory.
+There will be two types of players in the game, ‘real’ players and ‘computer’ players.
+Each player object should contain its own gameboard.
+*/
+
+export class Player {
+
+}
